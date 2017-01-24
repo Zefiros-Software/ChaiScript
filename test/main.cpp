07bd9aaf77b6b6f70d1a672f683265fe493071ff
PaulVisscher <paul.pev@hotmail.com>
2017-01-24 21:44:29 +0100

    Fix tests

--- test/main.cpp
+++ test/main.cpp
@@ -21,19 +21,16 @@
  */
 
 #include <chaiscript/chaiscript.hpp>
-#include <chaiscript/chaiscript_stdlib.hpp>
 
-double function(int i, double j)
+double function( int i, double j )
 {
-  return i * j;
+    return i * j;
 }
 
 int main()
 {
-  chaiscript::ChaiScript chai(chaiscript::Std_Lib::library());
-  chai.add(chaiscript::fun(&function), "function");
+    chaiscript::ChaiScript chai;
+    chai.add( chaiscript::fun( &function ), "function" );
 
-  double d = chai.eval<double>("function(3, 4.75);");
-
-  return 0;
+    double d = chai.eval<double>( "function(3, 4.75);" );
 }
--- test/zpm.lua
+++ test/zpm.lua
@@ -10,6 +10,4 @@ workspace "ChaiScript"
         kind "ConsoleApp"
         files "main.cpp"
 
-        zpm.uses {
-            "Zefiros-Software/ChaiScript"
-        }
+        zpm.uses "Zefiros-Software/ChaiScript"
