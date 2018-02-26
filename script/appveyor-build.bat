a72fa2189484237e03ff97cfd8c7d5c0ad9acc3d
PaulVisscher <paul.pev@hotmail.com>
2018-02-26 21:50:13 +0100

    Update to zpm 2

--- script/appveyor-build.bat
+++
@@ -1,7 +0,0 @@
-cd test/
-zpm install-package --allow-install --allow-module  || exit /b 1
-zpm vs2015 || exit /b 1
-
-msbuild ChaiScript.sln /property:Configuration=Test /property:Platform=x64 || exit /b 1
-
-.\bin\Test\ChaiScript.exe || exit /b 1
