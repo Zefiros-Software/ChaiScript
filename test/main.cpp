3f28c4f8f83de6d18918de50867c230a98e7ed18
p.e.visscher <p.e.visscher@zefiros.eu>
2018-05-31 20:04:31 +0200

    Update ci

--- test/main.cpp
+++ test/main.cpp
@@ -26,7 +26,7 @@
 
 #include <chaiscript/chaiscript.hpp>
 
-double function( int i, double j )
+double function(int i, double j)
 {
     return i * j;
 }
@@ -34,7 +34,7 @@ double function( int i, double j )
 int main()
 {
     chaiscript::ChaiScript chai;
-    chai.add( chaiscript::fun( &function ), "function" );
+    chai.add(chaiscript::fun(&function), "function");
 
-    double d = chai.eval<double>( "function(3, 4.75);" );
+    double d = chai.eval<double>("function(3, 4.75);");
 }
